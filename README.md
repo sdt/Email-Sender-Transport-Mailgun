272c32cf13e3753e873a3678fd19f088c277a760
Stephen Thirlwall <sdt@cpan.org>
2016-08-30 19:22:46 +1000

    minil new

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Email::Sender::Transport::Mailgun - It's new $module
+
+# SYNOPSIS
+
+    use Email::Sender::Transport::Mailgun;
+
+# DESCRIPTION
+
+Email::Sender::Transport::Mailgun is ...
+
+# LICENSE
+
+Copyright (C) Stephen Thirlwall.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Stephen Thirlwall <>
---
+++ lib/Email/Sender/Transport/Mailgun.pm
@@ -0,0 +1,39 @@
+package Email::Sender::Transport::Mailgun;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Email::Sender::Transport::Mailgun - It's new $module
+
+=head1 SYNOPSIS
+
+    use Email::Sender::Transport::Mailgun;
+
+=head1 DESCRIPTION
+
+Email::Sender::Transport::Mailgun is ...
+
+=head1 LICENSE
+
+Copyright (C) Stephen Thirlwall.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Stephen Thirlwall E<lt>E<gt>
+
+=cut
+
